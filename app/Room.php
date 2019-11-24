d87dca35cb18bcd522370e60d2cd59dcadb64ca0
Raphaël Thibierge <raphael@mbp.thibierge.dev>
2019-11-24 15:30:42 -0500

    Add Room model and relationships

---
+++ app/Room.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Room extends Model
+{
+    /**
+     * SQL table name
+     * @var string
+     */
+    protected $table = 'rooms';
+
+    /**
+     * Table fillable attributes
+     * @var array
+     */
+    protected $fillable = ['open', 'pin', 'slug'];
+
+    /**
+     * Date attributes to cast
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_ad'];
+
+    public function owner(): BelongsTo{
+        return $this->belongsTo('App\User', 'owner_id', 'id');
+    }
+
+    public function members(): BelongsToMany {
+        return $this
+            ->belongsToMany('App\User', 'room_members', 'room_id', 'user_id')
+            ->withTimestamps();;
+    }
+
+}
