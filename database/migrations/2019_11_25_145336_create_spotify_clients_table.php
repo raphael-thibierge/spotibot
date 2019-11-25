de80a6c00f734ef0bd26470c537f9d7d281d240d
Frenchy <aubin.puyoou@gmail.com>
2019-11-25 16:22:20 +0100

    Migration CreateSpotifyClientsTable

---
+++ database/migrations/2019_11_25_145336_create_spotify_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpotifyClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spotify_clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            //Spotify
+            $table->string('spotify_id');
+            $table->string('spotify_access_token')->nullable();
+            $table->string('spotify_refresh_token')->nullable();
+
+            //Other data
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spotify_clients');
+    }
+}
